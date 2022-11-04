f8e747bfe26abf2eab0770f0ce5470d4f734e1ab
Kirill Lyashenko <tltelf@yandex.ru>
2022-11-04 17:19:22 +0400

    Замечания исправлены

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,33 @@
-let editButton = document.querySelector('.profile__info-editButton');
+let buttonEdit = document.querySelector('.profile__info-edit-button');
 let popup = document.querySelector('.popup');
-let buttonClose = document.querySelector('.popup__buttonClose');
+let buttonClose = document.querySelector('.popup__button-close');
 let formElement = document.querySelector('.popup__container-form');
-let nameInput = document.querySelector('.popup__container-formField_name');
-let jobInput = document.querySelector('.popup__container-formField_job');
+let nameInput = document.querySelector('.popup__container-form-field_type_name');
+let jobInput = document.querySelector('.popup__container-form-field_type_job');
 let title = document.querySelector('.profile__info-title');
 let subtitle = document.querySelector('.profile__info-subtitle');
 
+//  Функция открытия поп-апа
 function openPopup() {
+  nameInput.value = title.textContent;
+  jobInput.value = subtitle.textContent;
   popup.classList.add('popup_opened');
 }
 
+//  Функция закрытия поп-апа
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
-editButton.addEventListener('click', openPopup);
-buttonClose.addEventListener('click', closePopup);
-
-function formSubmitHandler (evt) {
+//  Обработчик отправки формы
+function submitFormHandler (evt) {
   evt.preventDefault();
   title.textContent = nameInput.value;
   subtitle.textContent = jobInput.value;
   closePopup();
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
+buttonEdit.addEventListener('click', openPopup);
+buttonClose.addEventListener('click', closePopup);
+formElement.addEventListener('submit', submitFormHandler);
 
