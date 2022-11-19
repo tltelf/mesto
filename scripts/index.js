57241e894c53bf390ad2b5102800262a67133198
Kirill Lyashenko <tltelf@yandex.ru>
2022-11-19 17:54:03 +0400

    Ошибки исправлены

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,23 @@
 const buttonEdit = document.querySelector('.profile__info-edit-button');
 const buttonClose = document.querySelector('.popup__button-close');
 const formElement = document.querySelector('.popup__container-form');
-let nameInput = document.querySelector('.popup__container-form-field_type_name');
-let jobInput = document.querySelector('.popup__container-form-field_type_job');
-let title = document.querySelector('.profile__info-title');
-let subtitle = document.querySelector('.profile__info-subtitle');
+const nameInput = document.querySelector('.popup__container-form-field_type_name');
+const jobInput = document.querySelector('.popup__container-form-field_type_job');
+const title = document.querySelector('.profile__info-title');
+const subtitle = document.querySelector('.profile__info-subtitle');
 const buttonAdd = document.querySelector('.profile__add-button');
-let popups = document.querySelectorAll('.popup');
+const profilePopup = document.querySelector('.profile-popup');
+const cardPopup = document.querySelector('.card-popup')
+const photoPlacePopup = document.querySelector('.popup_photo-place');
 const buttonCloseNewPlace = document.querySelector('.popup__button-close_new-place');
 const formElementNewPlace = document.querySelector('.popup__container-form_new-place');
-let titleInput = document.querySelector('.popup__container-form-field_new-place-title');
-let linkInput = document.querySelector('.popup__container-form-field_new-place-link');
-let popupImg = document.querySelector('.popup__img_photo-place');
-let popupContainerTitle = document.querySelector('.popup__container-title-photo-place');
+const titleInput = document.querySelector('.popup__container-form-field_new-place-title');
+const linkInput = document.querySelector('.popup__container-form-field_new-place-link');
+const popupImg = document.querySelector('.popup__img-photo-place');
+const popupContainerTitle = document.querySelector('.popup__container-title-photo-place');
 const container = document.querySelector('.elements__cards');
 const cardTemplate = document.querySelector('#template').content.querySelector('.card');
+const closeButtons = document.querySelectorAll('.popup__button-close');
 
 const initialCards = [
   {
@@ -66,7 +69,7 @@ const generateCard = (dataCard) => {
   });
 
   image.addEventListener('click', function (evt) {
-    openPopup(2);
+    openPopup(photoPlacePopup);
     popupImg.src = evt.target.src;
     popupImg.alt = evt.target.alt;
     popupContainerTitle.textContent = evt.target.alt;
@@ -92,16 +95,16 @@ initialCards.forEach((dataCard) => {
 
 //  Функция открытия поп-апа
 
-function openPopup(index) {
+function openPopup(popup) {
   nameInput.value = title.textContent;
   jobInput.value = subtitle.textContent;
-  popups[index].classList.toggle('popup_opened');
+  popup.classList.add('popup_opened');
 }
 
 //  Функция закрытия поп-апа
 
-function closePopup(index) {
-  popups[index].classList.toggle('popup_opened');
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
 }
 
 //  Обработчик отправки формы
@@ -110,7 +113,7 @@ function submitFormHandler (evt) {
   evt.preventDefault();
   title.textContent = nameInput.value;
   subtitle.textContent = jobInput.value;
-  closePopup(0);
+  closePopup(profilePopup);
 }
 
 // Обработчик отправки формы добавления нового места
@@ -123,13 +126,17 @@ function submitFormHandlerNewPlace (evt) {
     });
   titleInput.value = '';
   linkInput.value = '';
-  closePopup(1);
+  closePopup(cardPopup);
 }
 
-buttonEdit.addEventListener('click', ()=> openPopup(0));
-buttonClose.addEventListener('click', ()=> closePopup(0));
-buttonAdd.addEventListener('click', ()=> openPopup(1));
-buttonCloseNewPlace.addEventListener('click', ()=> closePopup(1));
-document.querySelector('.popup__button-close_photo-place').addEventListener('click', ()=> closePopup(2));
+closeButtons.forEach((button) => {
+  // находим 1 раз ближайший к крестику попап 
+  const popup = button.closest('.popup');
+  // устанавливаем обработчик закрытия на крестик
+  button.addEventListener('click', () => closePopup(popup));
+});
+
+buttonEdit.addEventListener('click', ()=> openPopup(profilePopup));
+buttonAdd.addEventListener('click', ()=> openPopup(cardPopup));
 formElement.addEventListener('submit', submitFormHandler);
 formElementNewPlace.addEventListener('submit', submitFormHandlerNewPlace);
